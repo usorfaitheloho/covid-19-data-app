bd644013cd44a249001e34737453c80f00c3bf35
usorfaitheloho <usorfaitheloho@gmail.com>
2022-04-18 12:42:53 -0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import CountriesList from './component/CountriesList';
+import RegionList from './component/RegionList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+
+        <Routes>
+          <Route path="/countries" element={<RegionList />} />
+          <Route path="/" exact element={<CountriesList />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
