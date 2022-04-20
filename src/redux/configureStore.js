e7164f4887d5ecff4b3f454457f9ef1fefa9493c
usorfaitheloho <usorfaitheloho@gmail.com>
2022-04-20 05:00:38 -0700

    Add region reducer to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,16 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { countrieslistsReducer, initialState } from './reducer';
+import { regionsReducer, regionsState } from './regionReducer';
 
-const store = createStore(countrieslistsReducer,
-  initialState, composeWithDevTools(applyMiddleware(thunk)));
+const rootReducer = combineReducers({
+    countries: countrieslistsReducer,
+    regions: regionsReducer,
+});
+
+const store = createStore((state,action) => rootReducer(state,action),
+{ countries:initialState, regions:regionsState}, composeWithDevTools(applyMiddleware(logger, thunk)));
 
 export default store;
